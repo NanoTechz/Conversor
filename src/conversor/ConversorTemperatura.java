ec832d730f0d21b88c98576d5b8f33adc9e286ad
Nanson Augusto <nanson.augusto@hotmail.com>
2013-09-19 12:07:36 -0300

    Correção da lógica de um método

---
+++ src/conversor/ConversorTemperatura.java
@@ -0,0 +1,41 @@
+package conversor;
+
+/**
+ *
+ * @author Nanson Augusto
+ */
+
+public class ConversorTemperatura {
+
+    public int celsiusParaFahrenheit() {
+        return (9 * 40) / 5 + 32;
+    }
+
+    public int celsiusParaFahrenheit(int celsius) {
+        return (9 * celsius) / 5 + 32;
+    }
+
+    public double celsiusParaFahrenheit(double celsius) {
+        return (9.0 * celsius) / 5.0 + 32.0;
+    }
+
+    public double fahrenheitParaCelsius(double fahrenheit) {
+        return fahrenheit * (5.0 + 32.0) / 9.0;
+    }
+
+    public double kelvinParaCelsius(double kelvin) {
+        return kelvin - 273.0;
+    }
+
+    public double celsiusParaKelvin(double celsius) {
+        return celsius + 273.0;
+    }
+
+    public double kelvinParaFahrenheit(double kelvin) {
+        return celsiusParaFahrenheit(kelvin - 273.0);
+    }
+
+    public double fahrenheitParaKelvin(double fahrenheit) {
+        return celsiusParaKelvin(fahrenheitParaCelsius(fahrenheit));
+    }
+}
